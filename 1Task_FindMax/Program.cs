9f0f036842107f75d569755be98f35935bf0f642
Zhunus <samsungus071@gmail.com>
2022-07-12 13:12:43 +0600

    add new functions

--- 1Task_FindMax/Program.cs
+++ 1Task_FindMax/Program.cs
@@ -1,26 +1,22 @@
-﻿int a1 = 43;
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 43;
 int b1 = 176;
 int c1 = 26;
-int a2 = 81;
+int a2 = 8659;
 int b2 = 78;
 int c2 = 76;
 int a3 = 33;
-int b3 = 44;
+int b3 = 4434;
 int c3 = 67;
 
-int max = a1;
-
-if(b1 > max) max = b1;
-if(c1 > max) max = c1;
-
-if(a2 > max) max = a2;
-if(b2 > max) max = b2;
-if(c2 > max) max = c2;
-
-if(a3 > max) max = a3;
-if(b3 > max) max = b3;
-if(c3 > max) max = c3;
-
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 Console.WriteLine(max);
-
-
