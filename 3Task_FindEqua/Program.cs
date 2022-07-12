96fed6177e369ebba9649e3bd246d9942c6277b0
Zhunus <samsungus071@gmail.com>
2022-07-12 14:10:10 +0600

    add new TaskFindEqul

---
+++ 3Task_FindEqua/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1, 23, 31, 45, 54, 65, 14, 45};
+
+int n = array.Length;
+int find = 45;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array [index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
