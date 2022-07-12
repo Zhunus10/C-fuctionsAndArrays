a07b54b36e9eb73b7f7dabfc90360d6ae33134d3
Zhunus <samsungus071@gmail.com>
2022-07-12 13:45:44 +0600

    add new func Array

---
+++ 2Task_Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+//                0 1  2  3  4  5  6  7  8
+int [] array = {16,24,345,42,54,63,73,83,49};
+
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(result);
+
